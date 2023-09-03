789f25c9d2fd4031c02219b1b64d866317ed75b9
Iaggo Capitanio <iaggo.capitanio@gmail.com>
2023-09-02 22:38:50 -0300

    finished

--- olx/spiders/olx_spider.py
+++ olx/spiders/olx_spider.py
@@ -1,42 +1,42 @@
-from typing import List
-from  olx import settings
 import scrapy
 from scrapy.http import Response
-from scrapy.selector import Selector
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from olx.utils import get_page_number_from_url, get_error_path
+from selenium.webdriver.remote.webelement import WebElement
 from olx.items import OlxItem
-from olx.utils import get_page_number_from_url
-
+from olx import settings
 
 class OlxSpider(scrapy.Spider):
+    """
+    This spider uses Playwright to scroll the page and make all the items visible
+    """
     name = "olx"
     allowed_domains = ["olx.com.br"]
     ITEMS_XPATH = '//div[@id="main-content"]//div[contains(@class, "sc-bb3a36b6-0") and contains(@class, "bPPSiI")]'
     PRICE_XPATH = './/section/div[2]/div[1]/div[2]/h3'
     TITLE_XPATH = './/section/div[2]/div[1]/div[1]/a/h2'
     LINK_XPATH = './/section/div[2]/div[1]/div[1]/a'
-    DETAIL_XPATH = '//*[@id="content"]/div[2]/div/div[2]/div[1]/div[9]/div/div/div/div[2]/div/p/span'
+    DETAIL_XPATH = '//*[@id="content"]/div[2]/div/div[2]/div[1]/div[9]/div/div/div/div[2]/div/p/span/text()'
     start_urls = ["https://olx.com.br/imoveis/estado-sc"]
-    MAX_TIMEOUT = 180  # 3 minutes
 
-    def __init__(self,  *args, **kwargs):
+    MAX_TIMEOUT = 15
+
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.page_threshold = kwargs.get('page_threshold', settings.PAGE_THRESHOLD)
-
-    def start_requests(self):
-        for url in self.start_urls:
-            yield scrapy.Request(
-                url=url,
-                callback=self.parse,
-                errback=self.error_call_back,
-                meta={'playwright': True, "playwright_include_page": True}
-            )
-
-    async def error_call_back(self, failure):
-        if 'playwright_page' in failure.request.meta:
-            page = failure.request.meta['playwright_page']
-            await page.close()
-            self.logger.error(f"Error: {failure}")
+        options = webdriver.ChromeOptions()
+        for arg in kwargs.get('options', []):
+            options.add_argument(arg)
+        self.service = Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=self.service, options=options)
+        self.actions = ActionChains(self.driver)
+        self.page_threshold = int(kwargs.get('page_threshold', settings.PAGE_THRESHOLD ))
 
     @property
     def page_threshold(self) -> int:
@@ -46,63 +46,113 @@ class OlxSpider(scrapy.Spider):
     def page_threshold(self, value: int):
         self._page_threshold = value
 
-    async def parse(self, response, **kwargs):
-        items: List[Selector] = response.xpath(self.ITEMS_XPATH)
-        page = response.meta['playwright_page']
+    def parse_title(self, item: WebElement, wait=None) -> str:
+        return self._parse_element(item, wait, xpath=self.TITLE_XPATH)
 
-        for index, item in enumerate(items):
-            item_xpath = self.ITEMS_XPATH + f"[{index + 1}]"
+    def parse_price(self, item: WebElement, wait=None) -> str:
+        return self._parse_element(item, wait, xpath=self.PRICE_XPATH)
 
-            # Scroll to the current item. This makes sure the item is in view before extracting.
-            await page.eval_on_selector(item_xpath, "element => element.scrollIntoView()")
+    def parse_link(self, item: WebElement, wait=None) -> str:
+        return self._parse_element(item, wait, xpath=self.LINK_XPATH, tag_attr='href')
 
-            # Wait for the necessary elements of the item to be visible.
-            await page.wait_for_selector(f"{item_xpath}{self.PRICE_XPATH[2:]}", state='visible',
-                                         timeout=int(self.MAX_TIMEOUT * 1e3))
+    def parse_item_detail(self, response: Response, **kwargs) -> str:
+        title = response.meta.get('title', 'Not found')
+        price = response.meta.get('price', 'Not found')
+        description = response.xpath(self.DETAIL_XPATH).get()
+        yield OlxItem(description=description, title=title, price=price)
 
-            # Extract the details.
-            price = await page.eval_on_selector(f"{item_xpath}{self.PRICE_XPATH[2:]}", "element => element.textContent")
-            title = await page.eval_on_selector(f"{item_xpath}{self.TITLE_XPATH[2:]}", "element => element.textContent")
-            link = await page.eval_on_selector(f"{item_xpath}{self.LINK_XPATH[2:]}",
-                                               "element => element.getAttribute('href')")
-            print(f"{index} ,Price: {price}, Title: {title}, Link: {link}")
-            yield response.follow(url=link, callback=self.parse_detail,
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse, errback=self.error_call_back)
 
-                                  meta=dict(playwright=True,
-                                            playwright_include_page=True,
-                                            price=price,
-                                            title=title,
-                                            link=link,
-                                            ))
+    @staticmethod
+    async def error_call_back(failure):
+        """
+        This method is called when an error occurs.
+        :param failure:
+        :return:
+        """
+        error_path = get_error_path(failure)
+        page = failure.request.meta['playwright_page']
+        await page.screenshot(path=error_path)
+        await page.close()
+        print(f"Error: {failure}")
+
+    def parse(self, response: Response, **kwargs) -> None:
+        """
+        Parse the response and yield the items.
+        :param response:
+        :param kwargs:
+        :return:
+        """
+        self.driver.get(response.url)
+        wait = WebDriverWait(self.driver, self.MAX_TIMEOUT)
+        items = self.driver.find_elements(By.XPATH, self.ITEMS_XPATH)
+
+        for item in items[2:]:
+            self.actions.move_to_element(item).perform()
+            try:
+                price = self.parse_price(item, wait)
+                title = self.parse_title(item, wait)
+                link = self.parse_link(item, wait)
+
+                yield response.follow(url=link,
+                                      callback=self.parse_item_detail,
+                                      errback=self.error_call_back,
+                                      meta={'title': title, 'price': price}
+                                      )
+
+            except TimeoutException:
+                print("Timed out waiting for elements to become visible or couldn't find them for this item.")
 
         next_page_url = self.get_next_page_url(response)
         if next_page_url:
-            yield response.follow(url=next_page_url, callback=self.parse, meta=dict(playwright=True,
-                                                                                    playwright_include_page=True,
-                                                                                    ))
-        if 'playwright_page' in response.meta:
-            await response.meta['playwright_page'].close()
+            yield response.follow(url=next_page_url, callback=self.parse)
 
-    async def parse_detail(self, response: Response, **kwargs):
-        description = await response.meta['playwright_page'].eval_on_selector(
-            self.DETAIL_XPATH,
-            "element => element.textContent")
-        title = response.meta.get('title', 'Not found')
-        price = response.meta.get('price', 'Not found')
-        print(dict(description=description, title=title, price=price))
-        yield OlxItem(description=description, title=title, price=price)
-        if 'playwright_page' in response.meta:
-            await response.meta['playwright_page'].close()
-
-    def get_next_page_url(self, response):
-
+    def get_next_page_url(self, response: Response) -> str | None:
+        """
+        Get the next page url, it must respect a threshold of pages to be scraped.
+        :param response: The Scrapy Response object.
+        :return: It can return a string or None, if it returns None, it means that the threshold was reached or there is
+        no more content to be scraped.
+        """
+        content = response.xpath(self.ITEMS_XPATH)
         url = response.url
         number: int = get_page_number_from_url(url)
 
-        if number > self.page_threshold:
+        if not content or number > self.page_threshold:
             return None
 
         if number == 0:
             return f"{url}?o=2"
 
         return url.replace(f'o={number}', f'o={number + 1}')
+
+    def closed(self, reason):
+        """
+        This method is called when the spider is closed.
+        :param reason:
+        :return:
+        """
+        self.driver.quit()
+
+    def _parse_element(self, item: WebElement, wait: WebDriverWait | None = None, **kwargs) -> str:
+        """
+        Parse an element from the item, a general method to be used by other methods to parse elements.
+        :param item: A WebElement object
+        :param wait: A WebDriverWait object
+        :param kwargs: It can accept the following arguments: xpath, error_msg, tag
+        :return: it can return  the text of the element or the error_msg
+        """
+        xpath = kwargs.get('xpath')
+        error_msg: str = kwargs.get('error_msg', "Element Not Found")
+        tag_attr = kwargs.get('tag_attr', 'innerText')
+
+        if not wait:
+            wait = WebDriverWait(self.driver, self.MAX_TIMEOUT)
+        try:
+            element: WebElement = wait.until(EC.visibility_of(item.find_element(By.XPATH, xpath)))
+            return element.get_attribute(tag_attr) if element else error_msg
+        except TimeoutException:
+            print("Timed out waiting for elements to become visible or couldn't find them for this item.")
+            return error_msg
